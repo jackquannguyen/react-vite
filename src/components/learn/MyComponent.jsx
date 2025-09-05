df8c91e2ebb1905e243478aa2f54b624f6ac1eed
Nguyen Quan <hongquannguyenha@gmail.com>
2025-09-05 10:34:10 +0200

    video #31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -6,9 +6,21 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const quan = "hong quan";
+    // const quan = 22;
+    // const quan = true; //ko ra
+    // const quan = undefined;
+    // const quan = null; 
+    // const quan = [1,2,3]; //out: 123, JSON.stringify(quan) = [1,2,3]
+    const quan = {
+        name: "hong quan",
+        age: 22
+    }
+
     return (
         <>
-            <div>nguyen ha hong quan</div>
+            <div>nguyen ha {JSON.stringify(quan)} </div>
+            <div> {console.log("QUAN")} </div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
