dc6050ea3e35b7f5cb2eb127f0a3e088535eec47
Nguyen Quan <hongquannguyenha@gmail.com>
2025-09-05 10:01:13 +0200

    video #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,19 @@
 //() => { }
 //const MyComponent = () => { } thì chữ đầu phải viết hoa mới biên dịch đc Component
 //Component = html + css + js
+//JSX -> để return nhiều html thì phải đóng fragment (<> </>), cách này hay hơn đóng <div></div> vì nó ko ảnh hưởng layout khi cần css
+
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div>nguyen ha hong quan</div> //tag
+        <>
+            <div>nguyen ha hong quan</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
+
     )
 }
 
