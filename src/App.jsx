72e807ed6022ca188b1e894ad93ac092abd17d69
Nguyen Quan <hongquannguyenha@gmail.com>
2025-09-18 21:39:41 +0200

    video #34

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import ToDoNew from './components/todo/ToDoNew';
 import ToDoData from './components/todo/ToDoData';
 import reactLogo from './assets/react.svg';
 
+// 1) Data có thể được truyền từ Comp cha sang Comp con, và được lấy thông qua biến props (như App là comp cha, ToDoData là component con, props gồm 
+// {name, age, data} đc truyền từ App sang ToDoData)
 const App = () => {
 
+  const ten = "Quan Nguyen";
+  const age = 22;
+  const data = { country: "vietnam", city: "HCM" };
+
+  //{key:value}
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
         <ToDoNew />
-        <ToDoData />
+        <ToDoData
+          name={ten}
+          age={age}
+          data={data}
+        // phải khai báo key để lưu value truyền từ App, nếu ko sẽ ko gọi đc trong ToDoData.jsx
+        />
         <div className='todo-image'><img src={reactLogo} className='logo' /></div>
       </div>
 
--- src/components/todo/ToDoData.jsx
+++ src/components/todo/ToDoData.jsx
@@ -1,11 +1,27 @@
+// 1) props là 1 Object {}, do đó có thể khai báo: 
+// const {name, age, data} = props, ý là const name = props.name, const age = props.age, const data = props.data
+// 2) phải truyền props vào Funktionskopf thì mới gọi đc trong hàm
+// 3) còn nhiều cách truyền khác như {props.name} (ko cần k.báo const {name, age, data} = props) hay const ToDoData = ({name}) => {...}, tuy nhiên
+// beginner chưa nên học 
 
-const ToDoData = () => {
-    return (
-      <div className='todo-data'>
-        <div>Learning React</div>
-        <div>Watch youtube</div>
-      </div>
-    )
+//props là 1 Object {} có
+// {
+//   name: "Quan Nguyen",
+//   age: 22,
+//   data: {...}
+// }
+
+const ToDoData = (props) => {
+  const { name, age, data } = props;
+  console.log("Check props: ", props);
+
+  return (
+    <div className='todo-data'>
+      <div>Hello {name}</div>
+      <div>Learning React </div>
+      <div>Watch youtube</div>
+    </div>
+  )
 }
 
 export default ToDoData; 
